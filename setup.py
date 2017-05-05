b7c7019d1444faa4e5b8780fc8c55f2a3bce3afd
Julian Andrej <juan@tf.uni-kiel.de>
2017-05-05 08:43:08 +0200

    Finish setuppy

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="incflow",
+      version="0.1",
+      description="Incompressible Navier Stokes",
+      author="Julian Andrej",
+      url="",
+      packages=["incflow"])
