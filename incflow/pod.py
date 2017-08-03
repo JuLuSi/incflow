eac2a7cb840a3134ee2c892a4855b8b22a245016
Julian Andrej <juan@tf.uni-kiel.de>
2017-08-03 14:34:49 +0200

    Naming changes

--- incflow/pod.py
+++ incflow/pod.py
@@ -6,28 +6,28 @@ from slepc4py import SLEPc
 
 class POD():
     def __init__(self):
-        self.snapshots_mat = PETSc.Mat().create(PETSc.COMM_WORLD)
+        self.snapshot_mat = PETSc.Mat().create(PETSc.COMM_WORLD)
         self.full_basis_mat = PETSc.Mat().create(PETSc.COMM_WORLD)
         self.basis_mat = PETSc.Mat().create(PETSc.COMM_WORLD)
         self.L = 0
-        # self.snapshots_mat.setType('dense')
+        # self.snapshot_mat.setType('dense')
 
     def clear(self):
-        self.snapshots_mat.destroy()
+        self.snapshot_mat.destroy()
         self.full_basis_mat.destroy()
         self.basis_mat.destroy()
         self.L = 0
 
     def set_snapshot_matrix_size(self, Nx, Nt):
         # We need to call this before starting to store snapshots
-        self.snapshots_mat.setSizes([Nx, Nt])
-        self.snapshots_mat.setUp()
+        self.snapshot_mat.setSizes([Nx, Nt])
+        self.snapshot_mat.setUp()
 
     def store_one_snapshot(self, snap, l):
         # l is the index of the l-th column where we want to place the snapshot
-        self.snapshots_mat.setValues(
-            range(self.snapshots_mat.size[0]), l, snap.vector().array())
-        self.snapshots_mat.assemble()
+        self.snapshot_mat.setValues(
+            range(self.snapshot_mat.size[0]), l, snap.vector().array())
+        self.snapshot_mat.assemble()
 
     def compute(self, IP, D, eigenvalues_file, N, tol):
         """Compute projection matrix.
@@ -40,8 +40,8 @@ class POD():
             tol: Tolerance for the negligible part of complex value in the eigenvalues.
         """
 
-        Cp = D.matMult(IP.matMult(self.snapshots_mat.matMult(
-            D)).transposeMatMult(self.snapshots_mat))
+        Cp = D.matMult(IP.matMult(self.snapshot_mat.matMult(
+            D)).transposeMatMult(self.snapshot_mat))
 
         E = SLEPc.EPS()
         E.create()
@@ -95,23 +95,23 @@ class POD():
 
         ratio = eigs.getValues(range(self.L)).sum() / eigs.sum()
 
-        appo = PETSc.Vec().create(PETSc.COMM_WORLD)
-        appo.setSizes(Lmax)
-        appo.setUp()
-        self.full_basis_mat.setSizes([self.snapshots_mat.size[0], Lmax])
+        scaling_factors = PETSc.Vec().create(PETSc.COMM_WORLD)
+        scaling_factors.setSizes(Lmax)
+        scaling_factors.setUp()
+        self.full_basis_mat.setSizes([self.snapshot_mat.size[0], Lmax])
         self.full_basis_mat.setUp()
 
-        self.full_basis_mat = self.snapshots_mat.matMult(D.matMult(eigv))
+        self.full_basis_mat = self.snapshot_mat.matMult(D.matMult(eigv))
         for i in range(Lmax):
             print("lambda_", i, " = ", eigs.getValue(i))
-            appo.setValue(i, 1 / sqrt(eigs.getValue(i)))
-        self.full_basis_mat.diagonalScale(None, appo)
+            scaling_factors.setValue(i, 1 / sqrt(eigs.getValue(i)))
+        self.full_basis_mat.diagonalScale(None, scaling_factors)
 
-        self.basis_mat.setSizes([self.snapshots_mat.size[0], self.L])
+        self.basis_mat.setSizes([self.snapshot_mat.size[0], self.L])
         self.basis_mat.setUp()
 
-        self.basis_mat.setValues(range(self.snapshots_mat.size[0]), range(self.L),
-                                    self.full_basis_mat.getValues(range(self.snapshots_mat.size[0]),
+        self.basis_mat.setValues(range(self.snapshot_mat.size[0]), range(self.L),
+                                    self.full_basis_mat.getValues(range(self.snapshot_mat.size[0]),
                                                                      range(self.L)))
         self.basis_mat.assemble()
 
