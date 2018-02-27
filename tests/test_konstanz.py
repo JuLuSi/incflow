376c1162d9a077b015e2cb59a9c75e7104cac440
Julian Andrej <juan@tf.uni-kiel.de>
2018-02-27 11:04:42 +0100

    Added konstanz test case

---
+++ tests/test_konstanz.py
@@ -0,0 +1,64 @@
+from os.path import abspath, basename, dirname, join
+from firedrake import *
+from scipy.io import savemat
+import numpy as np
+from incflow import *
+
+cwd = abspath(dirname(__file__))
+data_dir = join(cwd, "data")
+
+mesh = Mesh(data_dir + "/room.e")
+mesh = MeshHierarchy(mesh, 1)[-1]
+
+incns = IncNavierStokes(mesh, nu=0.0005, rho=1.0)
+
+incns.dt = 0.1
+
+W = incns.get_mixed_fs()
+
+inflow_profile = (
+    "1.5 * 4.0 * x[1] * (0.5 - x[1]) / ( 0.5 * 0.5 )", "0.0")
+
+bcu_inflow = DirichletBC(W.sub(0), Expression(inflow_profile, degree=2), (1,))
+bcp_outflow = DirichletBC(W.sub(1), Constant(0.0), (2,))
+bcu_walls = DirichletBC(W.sub(0), Constant((0.0, 0.0)), (3,))
+
+incns.set_bcs(u_bcs=[bcu_inflow, bcu_walls],
+              p_bcs=[bcp_outflow])
+
+incns.setup_solver()
+
+outfile = File(join(data_dir, "../", "results/", "test_konstanz.pvd"))
+
+step = 0
+t = 0.0
+t_end = 0.5
+num_timesteps = int(t_end / incns.dt)
+output_frequency = 5
+
+print("Number of timesteps: {}".format(num_timesteps))
+print("Output frequency: {}".format(output_frequency))
+print("Number of output files: {}".format(int(num_timesteps / output_frequency)))
+print("INS DOFs: {}".format(incns.up0.vector().size()))
+
+snapshots = []
+
+while t <= t_end:
+    t += incns.dt
+
+    printp0("***********************")
+    printp0("Timestep {}".format(t))
+
+    u1, p1 = incns.step()
+
+    printp0("")
+
+    if step % output_frequency == 0:
+        outfile.write(u1, p1)
+
+    with u1.dat.vec as u1_loc:
+        snapshots.append(u1_loc.array)
+
+    step += 1
+
+savemat(data_dir + 'snapshots.mat', {'S': np.array(snapshots)})
