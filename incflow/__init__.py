4c670700aa1349a3eb33eda978a34dc42291b005
Julian Andrej <juland1111@gmail.com>
2018-08-09 09:59:04 +0200

    Added coupled to module

--- incflow/__init__.py
+++ incflow/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from incflow.util import *  # noqa
 from incflow.inc_navier_stokes import *  # noqa
+from incflow.inc_navier_stokes_coupled import *  # noqa
 from incflow.energy_eq import *  # noqa
 from incflow.morpc import *  # noqa
 from incflow.pod import *  # noqa
