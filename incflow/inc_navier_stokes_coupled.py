0105479e760e6af8abebd3061a0ee14955160e74
Julian Andrej <juan@tf.uni-kiel.de>
2018-09-28 14:02:37 +0200

    Fix for the fully coupled boussinesq

--- incflow/inc_navier_stokes_coupled.py
+++ incflow/inc_navier_stokes_coupled.py
@@ -3,7 +3,7 @@ from six.moves import map, range
 from firedrake import (Constant, Function, FunctionSpace, interpolate,
                        NonlinearVariationalProblem, NonlinearVariationalSolver, TestFunctions,
                        VectorFunctionSpace, div, nabla_grad, dot, dx, grad, inner, VectorSpaceBasis,
-                       CellVolume, split, MixedVectorSpaceBasis, as_vector, sqrt, MixedFunctionSpace, derivative)
+                       CellVolume, split, MixedVectorSpaceBasis, as_vector, sqrt, MixedFunctionSpace, derivative, projection)
 from .util import *
 
 
@@ -11,38 +11,81 @@ class IncNavierStokesEnEq(object):
     def __init__(self, mesh, nu, rho, cp, k):
         self.verbose = True
         self.mesh = mesh
-        self.dt = 0.001
+        self.dt = Constant(0.001)
         self.nu = nu
         self.rho = rho
         self.cp = cp
         self.k = k
         self.mu = self.nu * self.rho
         self.has_nullspace = False
+        self.time_discretization = 'BDF1'
+        self.td_theta = 0.5
 
-        self.V = VectorFunctionSpace(self.mesh, "CG", 2)
-        self.Q = FunctionSpace(self.mesh, "CG", 1)
+        P = 2
+        self.V = VectorFunctionSpace(self.mesh, "CG", P)
+        self.Q = FunctionSpace(self.mesh, "CG", P-1)
         self.S = FunctionSpace(self.mesh, "CG", 1)
         self.W = MixedFunctionSpace([self.V, self.Q, self.S])
 
-        self.forcing = Function(self.V)
+        self.forcing = Function(self.W.sub(0))
+
+        # self.solver_parameters = {
+        #     "mat_type": "aij",
+        #     "ksp_type": "gmres",
+        #     "ksp_gmres_restart": 300,
+        #     "pc_type": "asm",
+        #     "pc_asm_type": "restrict",
+        #     "pc_asm_overlap": 2,
+        #     "sub_ksp_type": "preonly",
+        #     "sub_pc_type": "ilu",
+        #     "sub_pc_factor_levels": 1,
+        # }
+
+        # self.solver_parameters = {
+        #     "mat_type": "aij",
+        #     # "snes_lag_jacobian": 1,
+        #     # "snes_lag_preconditioner": 1,
+        #     "ksp_type": "preonly",
+        #     "pc_type": "lu",
+        #     "pc_factor_mat_solver_type": "mumps"
+        # }
 
         self.solver_parameters = {
             "mat_type": "aij",
             "ksp_type": "fgmres",
-            "pc_type": "asm",
-            "pc_asm_type": "restrict",
-            "pc_asm_overlap": 1,
-            "sub_ksp_type": "preonly",
-            "sub_pc_type": "ilu",
-            "sub_pc_factor_levels": 1,
+            "ksp_gmres_restart": 300,
+            "pc_type": "fieldsplit",
+            "pc_fieldsplit_0_fields": "0,1",
+            "pc_fieldsplit_1_fields": "2",
+            "fieldsplit_0": {
+                "ksp_type": "gmres",
+                "ksp_rtol": 1e-5,
+                "pc_type": "fieldsplit",
+                "pc_fieldsplit_type": "schur",
+                "pc_fieldsplit_schur_fact_type": "full",
+                "pc_fieldsplit_schur_precondition": "self",
+                "fieldsplit_0": {
+                    "ksp_type": "gmres",
+                    "pc_type": "lu",
+                },
+                "fieldsplit_1": {
+                    "ksp_type": "gmres",
+                    "ksp_rtol": 1e-5,
+                    "pc_type": "lsc",
+                },                
+            },
+            # "fieldsplit_1": {
+            #     "ksp_type": "gmres",
+            #     "ksp_rtol": 1e-4,
+            #     "pc_type": "hypre",
+            #     "pc_hypre_boomeramg_P_max": 4,
+            #     "pc_hypre_boomeramg_agg_nl": 1,
+            #     "pc_hypre_boomeramg_agg_num_path": 2,
+            #     "pc_hypre_boomeramg_coarsen_type": "HMIS",
+            #     "pc_hypre_boomeramg_interp_type": "ext+i",
+            #     "pc_hypre_boomeramg_no_CF": True,
+            # },
         }
-        
-        # self.solver_parameters = {
-        #     "ksp_type": "gmres",
-        #     "mat_type": "aij",
-        #     "pc_type": "lu",
-        #     "pc_factor_mat_solver_type": "mumps"
-        # }
 
         if self.verbose:
             self.solver_parameters["snes_monitor"] = True
@@ -55,12 +98,18 @@ class IncNavierStokesEnEq(object):
         self.upT0 = Function(self.W)
         self.u0, self.p0, self.T0 = split(self.upT0)
 
-        self.upT = Function(self.W)
-        self.u1, self.p1, self.T1 = split(self.upT)
+        self.upT1 = Function(self.W)
+        self.u1, self.p1, self.T1 = split(self.upT1)
 
-        self.upT0.sub(0).rename("velocity")
-        self.upT0.sub(1).rename("pressure")
-        self.upT0.sub(2).rename("temperature")
+        self.upT2 = Function(self.W)
+        self.u2, self.p2, self.T2 = split(self.upT2)
+
+        self.upTmr = None  # Most recent timestep
+
+        for block in [self.upT0, self.upT1, self.upT2]:
+            block.sub(0).rename("velocity")
+            block.sub(1).rename("pressure")
+            block.sub(2).rename("temperature")
 
         v, q, s = TestFunctions(self.W)
 
@@ -76,60 +125,98 @@ class IncNavierStokesEnEq(object):
         tau = ((2.0 / self.dt) ** 2 + (2.0 * u_norm / h)
                ** 2 + (4.0 * self.nu / h ** 2) ** 2) ** (-0.5)
 
-        # temporal discretization
-        self.F = (1.0 / self.dt) * inner(self.u1 - self.u0, v) * dx
-
-        # weak form
-        self.F += (
-                + inner(dot(self.u1, nabla_grad(self.u1)), v) * dx
-                + self.nu * inner(grad(self.u1), grad(v)) * dx
-                - (1.0 / self.rho) * self.p1 * div(v) * dx
-                + div(self.u1) * q * dx
-                - inner(self.forcing, v) * dx
-        )
-
-        if stabilization:
-            # GLS
-
-            # residual form
-            R = (
-                    + (1.0 / self.dt) * (self.u1 - self.u0)
-                    + dot(self.u0, nabla_grad(self.u1))
-                    - self.nu * div(grad(self.u1))
-                    + (1.0 / self.rho) * grad(self.p1)
-                    - self.forcing
+        def ins_form(u, p, T):
+            F = (
+                    + inner(dot(u, nabla_grad(u)), v) * dx
+                    + self.nu * inner(grad(u), grad(v)) * dx
+                    - (1.0 / self.rho) * p * div(v) * dx
+                    + div(u) * q * dx
+                    - inner(self.forcing, v) * dx
             )
+            return F
 
-            self.F += tau * inner(
-                + dot(self.u0, nabla_grad(v))
-                - self.nu * div(grad(v))
-                + (1.0 / self.rho) * grad(q), R) * dx
+        def eneq_form(u, T):
+            F = (
+                    + self.rho * self.cp * inner(dot(u, grad(T)), s) * dx
+                    + self.k * inner(grad(T), grad(s)) * dx
+            )
+            return F
 
-        self.F += self.rho * self.cp * (1.0 / self.dt) * inner((self.T1 - self.T0), s) * dx
-        
-        self.F += (
-            self.rho * self.cp * inner(dot(self.u1, grad(self.T1)), s) * dx
-            + self.k * inner(grad(self.T1), grad(s)) * dx
-        )
+        if self.time_discretization == 'BDF1':
+            self.upTmr = self.upT1
 
-        self.problem = NonlinearVariationalProblem(self.F, self.upT, self.bcs)
+            self.F = 1.0 / self.dt * inner(self.u1 - self.u0, v) * dx
+            self.F += ins_form(self.u1, self.p1, self.T1)
+
+            self.F += 1.0 / self.dt * self.rho * self.cp * inner((self.T1 - self.T0), s) * dx
+            self.F += eneq_form(self.u1, self.T1)
+        elif self.time_discretization == 'BDF2':
+            self.upTmr = self.upT2
+
+            self.F = Constant(1.0 / (2.0 * self.dt)) * inner(3.0 * self.u2 - 4.0 * self.u1 + self.u0, v) * dx
+            self.F += ins_form(self.u2, self.p2, self.T2)
+
+            self.F += Constant(1.0 / (2.0 * self.dt)) * inner(3.0 * self.T2 - 4.0 * self.T1 + self.T0, s) * dx
+            self.F += eneq_form(self.u2, self.T2)
+        elif self.time_discretization == 'Theta':
+            self.upTmr = self.upT1
+
+            self.F = Constant(1.0 / self.dt) * inner(self.u1 - self.u0, v) * dx
+            self.F += Constant(self.td_theta) * ins_form(self.u1, self.p1, self.T1)
+            self.F += Constant(1.0 - self.td_theta) * ins_form(self.u0, self.p0, self.T0)
+
+            self.F += Constant(1.0 / self.dt) * self.rho * self.cp * inner((self.T1 - self.T0), s) * dx
+            self.F += Constant(self.td_theta) * eneq_form(self.u1, self.T1)
+            self.F += Constant(1.0 - self.td_theta) * eneq_form(self.u0, self.T0)
+
+            # if stabilization:
+        #     # GLS
+        #
+        #     # residual form
+        #     R = (
+        #             + (1.0 / self.dt) * (self.u1 - self.u0)
+        #             + dot(self.u0, nabla_grad(self.u1))
+        #             - self.nu * div(grad(self.u1))
+        #             + (1.0 / self.rho) * grad(self.p1)
+        #             - self.forcing
+        #     )
+        #
+        #     self.F += tau * inner(
+        #         + dot(self.u0, nabla_grad(v))
+        #         - self.nu * div(grad(v))
+        #         + (1.0 / self.rho) * grad(q), R) * dx
+
+        self.problem = NonlinearVariationalProblem(self.F, self.upTmr, self.bcs)
         self.solver = NonlinearVariationalSolver(
             self.problem,
+            options_prefix='coupled',
             nullspace=nullspace,
             solver_parameters=self.solver_parameters)
 
     def get_mixed_fs(self):
         return self.W
 
-    def set_forcing(self, f):
-        self.forcing.project(f)
+    def build_forcing_projector(self, f):
+       	self.forcing_projector = projection.Projector(f, self.forcing)
+
+    def set_forcing(self):
+        self.forcing_projector.project()
 
     def set_bcs(self, u_bcs, p_bcs, T_bcs):
         self.bcs = list(chain.from_iterable([u_bcs, p_bcs, T_bcs]))
 
-    def step(self):
+    def solve(self):
         if self.verbose:
             printp0("IncNavierStokesEnEq")
         self.solver.solve()
-        self.upT0.assign(self.upT)
-        return self.upT0.split()
+
+    def step(self):
+        if self.time_discretization == 'BDF1':
+            self.upT0.assign(self.upT1)
+        elif self.time_discretization == 'BDF2':
+            self.upT0.assign(self.upT1)
+            self.upT1.assign(self.upT2)
+        elif self.time_discretization == 'Theta':
+            self.upT0.assign(self.upT1)
+
+        return self.upTmr.split()
---
+++ tests/test_coupled.py
@@ -0,0 +1,120 @@
+from os.path import abspath, basename, dirname, join
+from firedrake import *
+from incflow import *
+import numpy as np
+import pytest
+
+set_log_level(DEBUG)
+
+cwd = abspath(dirname(__file__))
+data_dir = join(cwd, "data")
+
+H = 8.0
+L = 1.0
+Pr = 0.71
+Ra = 3.4e5
+mu = Pr
+k = 1.0
+rho = 1.0
+cp = 1.0
+g = 1.0
+T_cold = Constant(-0.5)
+T_hot = Constant(+0.5)
+
+N = 100
+mesh = RectangleMesh(N / 4, N, L, H)
+x, y = SpatialCoordinate(mesh)
+
+DG0 = FunctionSpace(mesh, "DG", 0)
+
+write_csv = True
+
+model = IncNavierStokesEnEq(mesh, nu=sqrt(Pr / Ra), rho=rho, k=1.0 / sqrt(Ra * Pr), cp=cp)
+model.dt.assign(0.005)
+
+W = model.get_mixed_fs()
+
+bcu_walls = DirichletBC(W.sub(0), Constant((0.0, 0.0)), (1, 2, 3, 4))
+
+bcT = [
+    DirichletBC(W.sub(2), T_cold, (2,)),
+    DirichletBC(W.sub(2), T_hot, (1,))
+]
+
+p_pin = DirichletBC(W.sub(1), Constant(0), (1,))
+p_pin.nodes = p_pin.nodes[:1]
+
+model.set_bcs(u_bcs=[bcu_walls],
+              p_bcs=[],
+              T_bcs=bcT)
+
+model.has_nullspace = True
+
+model.setup_solver(stabilization=False)
+
+outfile = File(join(data_dir, "../", "results/", "test_temp_driven_cavity.pvd"))
+
+model.upT0.sub(2).project(Constant(0.0))
+
+step = 0
+t = 0.0
+t_end = 100.0
+num_timesteps = int(t_end / float(model.dt))
+output_frequency = 100
+
+print("Number of timesteps: {}".format(num_timesteps))
+print("Output frequency: {}".format(output_frequency))
+print("Number of output files: {}".format(int(num_timesteps / output_frequency)))
+print("DOFs: {}".format(model.upT0.vector().size()))
+
+if write_csv:
+    csv_file = open('mit81_points.csv', 'w', newline='')
+    csv_file.write('time Tp1 up1 vp1 Nu0\n')
+
+n = FacetNormal(mesh)
+nusselt = dot(grad(model.upT0.sub(2)), n)
+
+model.build_forcing_projector(model.T1 * as_vector([Constant(0.0), Constant(1.0)]))
+
+while t <= t_end:
+    with Timer('Step'):
+        t += float(model.dt)
+
+        printp0("***********************")
+        printp0("Time {:.3E}".format(t))
+        printp0("Timestep {:.3E}".format(float(model.dt)))
+        
+        model.set_forcing()
+
+        model.solve()
+
+        u1, p1, T1 = model.step()
+
+        Tp1 = T1.at(0.181, 7.37, tolerance=1e-8)
+        up1 = u1.at(0.181, 7.37, tolerance=1e-8)[0]
+        vp1 = u1.at(0.181, 7.37, tolerance=1e-8)[1]
+        Nu0 = assemble(Constant(1.0 / H) * nusselt * ds(1))
+
+        cfl = project(sqrt(inner(u1, u1)) * model.dt / CellSize(mesh), DG0)
+        max_cfl = np.max(cfl.vector().array())
+           
+        print('CFL: {:+.5E}'.format(max_cfl))
+
+        cfl_max = 1.0
+        delta_dt = 0.01
+        if max_cfl >= cfl_max or float(model.dt) > 0.1:
+            model.dt.assign(float(model.dt) - delta_dt)
+        else:
+            model.dt.assign(float(model.dt) + delta_dt)
+
+        if write_csv:
+            csv_file.write('{:+.5E} {:+.5E} {:+.5E} {:+.5E} {:+.5E}\n'.format(t, Tp1, up1, vp1, Nu0))
+
+        if step % output_frequency == 0:
+            outfile.write(u1, p1, T1, time=t)
+            if write_csv:
+                csv_file.flush()
+
+        step += 1
+
+    printp0("")
